da75540a0c51f4da5464be1b88af6229235c51c1
jhuizy <jhuizenga99@gmail.com>
2018-03-28 13:18:16 +0800

    added docker compose and sequelize

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialiect: 'postgres'
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
